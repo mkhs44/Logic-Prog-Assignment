68bef16738fec130923de86dca67f38f3353e132
mkhs <mkh_sharif@aol.com>
2017-02-08 00:11:24 -0500

    Question 1: adds sum and checks if equal to N

--- lp.pl
+++ lp.pl
@@ -1,5 +1,8 @@
 sum-up-numbers-simple(L, N) :-        /* define function */
-
+  sum-up-list(L, Value),              /* recursive call */
+  Value is N.                         /* check if value is equal to N */
 
 sum-up-list([], 0).                   /* declare helper function */
 sum-up-list([First|Last], Value) :-
+  sum-up-list(Last, SumLast),         /* pass in next value and recurse */
+  Value is First + SumLast.           /* add values */
