6dca9aac5c0149ea92725f3d102c7bded82fe71f
mkhs <mkh_sharif@aol.com>
2017-02-07 23:47:45 -0500

    Question 1: define helper function

--- lp.pl
+++ lp.pl
@@ -1,1 +1,5 @@
-sum-up-numbers-simple(L, N) :- /* define function */
+sum-up-numbers-simple(L, N) :-        /* define function */
+
+
+sum-up-list([], 0).                   /* declare helper function */
+sum-up-list([First|Last], Value) :-
