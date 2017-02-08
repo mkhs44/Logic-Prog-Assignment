5156bee2b5469a131e6bc8ee0d93672abfba553b
mkhs <mkh_sharif@aol.com>
2017-02-08 01:27:07 -0500

    Question 2: created helper function

--- lp.pl
+++ lp.pl
@@ -1,14 +1,19 @@
-sum-up-numbers-simple(L, N) :-        /* define sum simple function */
-  sum-up-list(L, Value),              /* recursive call */
-  Value is N.                         /* check if value is equal to N */
+/* Question 1 */
+sum-up-numbers-simple(L, N) :-          /* define sum simple function */
+  sum-up-list(L, Value),                /* recursive call */
+  Value is N.                           /* check if value is equal to N */
 
-sum-up-numbers-general(L, N) :-       /* define sum general function */
-
-sum-up-list([], 0).                   /* declare helper function */
-sum-up-list([First|Last], Value) :-
+sum-up-list([], 0).                     /* if list is empty, return 0 */
+sum-up-list([First|Last], Value) :-     /* declare helper function */
     number(First),                      /* check if value is a number */
     sum-up-list(Last, SumLast),         /* pass in next value and recurse */
     Value is First + SumLast.           /* add values */
 sum-up-list([First|Last], Value) :-
   \+number(First),                      /* check if value is not a number */
   sum-up-list(Last, Value).             /* if value is not a number, emit */
+
+/* Question 2 */
+sum-up-numbers-general(L, N) :-         /* define sum general function */
+
+sum-up-list-gen([], 0).                 /* if list is empty, return 0 */
+sum-up-list-gen([First|Last], Value) :-
