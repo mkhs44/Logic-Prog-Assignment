274ffe6565e9a9f9e7cc7acda8855212346a6cce
mkhs <mkh_sharif@aol.com>
2017-02-08 01:02:40 -0500

    Question 1: function handles letters

--- lp.pl
+++ lp.pl
@@ -4,7 +4,9 @@ sum-up-numbers-simple(L, N) :-        /* define function */
 
 sum-up-list([], 0).                   /* declare helper function */
 sum-up-list([First|Last], Value) :-
+    number(First)                       /* check if value is a number */
     sum-up-list(Last, SumLast),         /* pass in next value and recurse */
     Value is First + SumLast.           /* add values */
 sum-up-list([First|Last], Value) :-
-  \+number(First),                      /* check value is not a number */           
+  \+number(First),                      /* check if value is not a number */
+  sum-up-list(Last, Value).             /* if value is not a number, emit */
